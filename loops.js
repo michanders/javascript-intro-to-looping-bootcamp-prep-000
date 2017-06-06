0cd1cd1261809c714b2bb60a5000da889c97ff83
Michael Anderson <michanders@msn.com>
2017-06-06 22:21:41 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,16 +1,15 @@
-
-
 function forLoop(array){
-  for(let i=0; i<=25; i++){
+  for(let i=0; i<25; i++){
     if(i!=1){
-      array[i]= ("I am ${i} strange loops.");
+      array.push(`I am ${i} strange loops.`);
     }
     else{
-      array[i]= ("I am " + 1 + " strange loop.");
+      array.push(`I am ${i} strange loop.`);
     }
   }
   return(array);
 }
+
 
 function whileLoop(n){
   while(n>0){
